5dd9045515bc566af0779d5aa41667c9f0ab25d6
alexandre <amenezes031@gmail.com>
2019-12-31 12:21:11 -0300

    Add button features (Play/Pause, Volume, Mute and Seekbar).

--- base/player.js
+++ base/player.js
@@ -1,29 +1,77 @@
-const player = {
-  cover: document.querySelector(".card-image"),
-  title: document.querySelector(".card-content h5"),
-  artist: document.querySelector(".artist"),
-  audio: document.querySelector("audio"),
+import audios from "./data.js";
+import { path } from "./utils.js";
+import elements from "./playerElements.js";
+import { secondsToMinutes } from "./utils.js"
+
+export default {
   audioData: audios,
   currentAudio: {},
   currentPlaying: 0,
-  start() {
-    this.update();
+  isPlaying: false,
 
-    this.audio.onended = () => this.next();
+  start() {
+    elements.get.call(this);
+
+    this.update();
   },
+
+  play() {
+    this.isPlaying = true;
+    this.audio.play();
+    this.playPause.innerText = "pause";
+  },
+
+  pause() {
+    this.isPlaying = false;
+    this.audio.pause();
+    this.playPause.innerText = "play_arrow";
+  },
+
+  togglePlayPause() {
+    if (this.isPlaying) {
+      this.pause()
+    } else {
+      this.play();
+    }
+  },
+
+  toggleMute() {
+    this.audio.muted = !this.audio.muted;
+    this.mute.innerText = this.audio.muted ? "volume_mute" : "volume_up";
+  },
+
+  setVolume(value) {
+    this.audio.volume = value / 100;
+  },
+
+  setSeek(value) {
+    this.audio.currentTime = value;
+  },
+
+  timeUpdate() {
+    this.currentDuration.innerText = secondsToMinutes(this.audio.currentTime);
+    this.seekbar.value = this.audio.currentTime;
+  },
+
   next() {
     this.currentPlaying++;
     if (this.currentPlaying == this.audioData.length) this.restart();
     this.update();
+    this.play();
   },
+
   update() {
     this.currentAudio = this.audioData[this.currentPlaying];
 
     this.cover.style.background = `url('${path(this.currentAudio.cover)}') no-repeat center center / cover`;
     this.title.innerText = this.currentAudio.title;
     this.artist.innerText = this.currentAudio.artist;
-    this.audio.src = path(this.currentAudio.file);
+    elements.createAudioElement.call(this, path(this.currentAudio.file));
+    this.audio.onloadeddata = () => {
+      elements.actions.call(this);
+    };
   },
+
   restart() {
     this.currentPlaying = 0;
     this.update();
